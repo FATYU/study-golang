16ac83b61071ec08fe808b4983af362a09ecda70
Z <33862677@qq.com>
2021-08-17 23:21:14 +0800

    :globe_with_meridians: http

--- oom.world/ch1/http/httpserver.go
+++ oom.world/ch1/http/httpserver.go
@@ -14,3 +14,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 	fmt.Fprintf(w, "Header connection = %q\n", r.Header["Connection"])
 }
+
+//如果需要后台运行,可以执行的时候,脚本添加 & . 比如 go run main.go &
