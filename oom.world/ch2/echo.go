682cadb1717e5103d1e4fe56031a8fabe08f060c
Z <33862677@qq.com>
2021-08-19 23:59:46 +0800

    :)

---
+++ oom.world/ch2/echo.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var blankline = flag.Bool("n", false, "忽略行尾换行符")
+var sep = flag.String("sep", "/", "连接符")
+
+func main() {
+	flag.Parse()
+	fmt.Println(strings.Join(flag.Args(), " "))
+
+	if !*blankline {
+		fmt.Println() //打印空行
+	}
+
+	fmt.Println("=====================")
+}
